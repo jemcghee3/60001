66ebb66b738df4143baa6ca216cc2bded348c240
James McGhee <59738821+jemcghee3@users.noreply.github.com>
2022-02-20 19:18:22 +0000

    initial commit ch5fe1.py

---
+++ ch5fe1.py
@@ -0,0 +1,10 @@
+# Introduction to Computation and Programming Using Python (2021)
+# Chapter 5, Finger Exercise 1
+"""
+Write an expression that evaluates to the mean of
+a tuple of numbers. Use the function sum.
+"""
+
+num = (0, 1, -1, 2, 3, 5)
+
+print(sum(num)/len(num))
