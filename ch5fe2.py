9ba95b0c931b43fcc99be2773712393708948be9
James McGhee <59738821+jemcghee3@users.noreply.github.com>
2022-04-26 14:02:31 +0000

    ch5fe2.py initial commit

---
+++ ch5fe2.py
@@ -0,0 +1,5 @@
+# Introduction to Computation and Programming Using Python (2021)
+# Chapter 5, Finger Exercise 2
+"Write a list comprehension that generates all non-primes between 2 and 100."
+
+print([x for x in range(2, 100) if any(x % y == 0 for y in range(3, x))])
